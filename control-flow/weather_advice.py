ced13f0ce9de448c5f4ca58e63a56c491af7d362
Bianca Laura <biancamakio02@gmail.com>
2025-05-31 21:36:45 +0300

    Learning about control-flow

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,10 @@
+weather_input = str(input("What's the weather like today? (sunny/rainy/cloudy): "))
+
+if weather_input == "sunny":
+    print("Wear a t-shirt and sunglasses.")
+elif weather_input == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+elif weather_input == "cloudy":
+    print("Make sure to wear a warm coat and a scarf.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")

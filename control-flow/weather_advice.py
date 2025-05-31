a0b9b335ea9a25dffd6cb6fbac4554d9455215bf
Bianca Laura <biancamakio02@gmail.com>
2025-05-31 22:49:54 +0300

    just removed the cold from the file inside control-flow

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,10 +1,10 @@
 weather_input = (input("what's the weather like today? (sunny/rainy/cold): "))
 
-if weather_input == "sunny":
+if weather_input == 'sunny':
     print("wear a t-shirt and sunglasses.")
-elif weather_input == "rainy":
+elif weather_input == 'rainy':
     print("don't forget your umbrella and a raincoat.")
-elif weather_input == "cloudy":
+elif weather_input == 'cold':
     print("make sure to wear a warm coat and a scarf.")
 else:
     print("sorry, I don't have recommendations for this weather.")

eb86faa5a0cd4c4e1005818b7021625492d95fd8
Bianca Laura <biancamakio02@gmail.com>
2025-06-15 21:19:36 +0300

    changed subtract to subtraction

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -12,7 +12,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(-5, -5), -10)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(10, 4), 6)
         self.assertEqual(self.calc.subtract(0, 10), -10)
         self.assertEqual(self.calc.subtract(-3, -7), 4)
